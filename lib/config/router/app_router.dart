47ef701eee998f71db73beca2630cc49fb0b88d0
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-02-05 19:45:51 -0500

    fix config

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:go_router/go_router.dart';
+import '../../presentation/screens/screens.dart';
+
+
+final appRouter = GoRouter(
+  initialLocation: '/loading',
+  routes: [
+    ///* Auth Routes
+    GoRoute(
+      path: '/gps_access',
+      builder: (context, state) =>  const GpsAccessScreen(),
+    ),
+    GoRoute(
+      path: '/loading',
+      builder: (context, state) =>  const LoadingScreen(),
+    ),
+    GoRoute(
+      path: '/map',
+      builder: (context, state) =>  const MapScreen(),
+    ),
+  ],
+);
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'config/config.dart';
+import 'presentation/blocs/blocs.dart';
+
+void main() {
+  runApp(
+    MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (context) => GpsBloc()),
+
+    ], child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).theme(),
+      title: 'Material App',
+      routerConfig: appRouter,
+    );
+  }
+}
